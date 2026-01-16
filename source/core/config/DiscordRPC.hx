dfab305cc188a086f0e6e83624e7b80409100ec0
KirbyKidJ <70983335+KirbyKid256@users.noreply.github.com>
2026-01-16 10:39:54 -0800

    Fixed Discord RPC Setting
    This PR adds a couple of fixes for the Discord RPC and when you change the setting in the game.
    1. The RPC tries to initializes AFTER all the settings are loaded, meaning if you turned it off, the RPC won't initialize.
    2. Added a new function in the class to check if the RPC should automatically turn on or off. Currently it's only used in `DiscordRPC.changePresence` and `DiscordRPC.updatePresence`

--- source/core/config/DiscordRPC.hx
+++ source/core/config/DiscordRPC.hx
@@ -16,117 +16,134 @@ import lime.app.Application;
 
 class DiscordRPC
 {
-    public static var initialized:Bool = false;
+	public static var initialized:Bool = false;
 
-    private static var presence:#if DISCORD_ALLOWED DiscordRichPresence = new DiscordRichPresence() #else Dynamic = null #end;
+	private static var presence:#if DISCORD_ALLOWED DiscordRichPresence = new DiscordRichPresence() #else Dynamic = null #end;
 
-    @:unreflective private static var thread:Thread;
+	@:unreflective private static var thread:Thread;
 
-    public static function initialize(id:String)
-    {
+	public static function check():Bool
+	{
+		if (ClientPrefs.data.discordRPC)
+		{
+			if (!initialized) DiscordRPC.initialize(CoolVars.data.discordID);
+		} else {
+			if (initialized) shutdown();
+		}
+		return ClientPrefs.data.discordRPC;
+	}
+
+	public static function initialize(id:String)
+	{
 		#if DISCORD_ALLOWED
-        if (!ClientPrefs.data.discordRPC)
-            return;
+		if (!ClientPrefs.data.discordRPC)
+		{
+			if (initialized) shutdown();
+			return;
+		}
 
-        var eventHandlers:DiscordEventHandlers = new DiscordEventHandlers();
-        eventHandlers.ready = Function.fromStaticFunction(onReady);
-        eventHandlers.disconnected = Function.fromStaticFunction(onDisconnected);
-        eventHandlers.errored = Function.fromStaticFunction(onError);
+		var eventHandlers:DiscordEventHandlers = new DiscordEventHandlers();
+		eventHandlers.ready = Function.fromStaticFunction(onReady);
+		eventHandlers.disconnected = Function.fromStaticFunction(onDisconnected);
+		eventHandlers.errored = Function.fromStaticFunction(onError);
 
-        Discord.Initialize(id, RawPointer.addressOf(eventHandlers), false, null);
+		Discord.Initialize(id, RawPointer.addressOf(eventHandlers), false, null);
 
-        if (thread == null)
-        {
-            thread = Thread.create(function():Void
-                {
-                    while (true)
-                    {
-                        #if DISCORD_DISABLE_IO_THREAD
-                        Discord.UpdateConnection();
-                        #end
+		if (thread == null)
+		{
+			thread = Thread.create(function():Void
+				{
+					while (true)
+					{
+						#if DISCORD_DISABLE_IO_THREAD
+						Discord.UpdateConnection();
+						#end
 
-                        Discord.RunCallbacks();
+						Discord.RunCallbacks();
 
-                        Sys.sleep(1);
-                    }
-                }
-            );
-        }
+						Sys.sleep(1);
+					}
+				}
+			);
+		}
 
 		Application.current.window.onClose.add(function()
-            {
-			    if (initialized)
-                    shutdown();
-		    }
-        );
+			{
+				if (initialized)
+					shutdown();
+			}
+		);
 
-        initialized = true;
+		initialized = true;
 		#end
-    }
+	}
 
-    public static function shutdown()
-    {
+	public static function shutdown()
+	{
 		#if DISCORD_ALLOWED
-        initialized = false;
+		initialized = false;
 
-        Discord.Shutdown();
+		Discord.Shutdown();
 		#end
-    }
+	}
 
-    public static function changePresence(details:String, ?state:String, ?largeImage:String, ?smallImage:String, ?usesTime:Bool = false, ?endTime:Float = 0)
-    {
+	public static function changePresence(details:String, ?state:String, ?largeImage:String, ?smallImage:String, ?usesTime:Bool = false, ?endTime:Float = 0)
+	{
 		#if DISCORD_ALLOWED
-        var startTime:Float = 0;
+		if (!check()) return;
 
-        if (usesTime)
-            startTime = Date.now().getTime();
+		var startTime:Float = 0;
 
-        if (endTime > 0)
-            endTime = startTime + endTime;
+		if (usesTime)
+			startTime = Date.now().getTime();
 
-        presence.state = state;
-        presence.details = details;
-        presence.largeImageKey = largeImage;
-        presence.largeImageText = 'Engine Version: ' + CoolVars.engineVersion;
-        presence.smallImageKey = smallImage;
-        presence.startTimestamp = Std.int(startTime / 1000);
-        presence.endTimestamp = Std.int(endTime / 1000);
+		if (endTime > 0)
+			endTime = startTime + endTime;
 
-        updatePresence();
+		presence.state = state;
+		presence.details = details;
+		presence.largeImageKey = largeImage;
+		presence.largeImageText = 'Engine Version: ' + CoolVars.engineVersion;
+		presence.smallImageKey = smallImage;
+		presence.startTimestamp = Std.int(startTime / 1000);
+		presence.endTimestamp = Std.int(endTime / 1000);
+
+		updatePresence();
 		#end
-    }
+	}
 
-    public static function updatePresence()
-    {
+	public static function updatePresence()
+	{
 		#if DISCORD_ALLOWED
-        Discord.UpdatePresence(RawConstPointer.addressOf(presence));
+		if (!check()) return;
+		Discord.UpdatePresence(RawConstPointer.addressOf(presence));
 		#end
-    }
+	}
 
-    private static function onReady(request:#if DISCORD_ALLOWED RawConstPointer<DiscordUser> #else Dynamic #end):Void
-    {
+	private static function onReady(request:#if DISCORD_ALLOWED RawConstPointer<DiscordUser> #else Dynamic #end):Void
+	{
 		#if DISCORD_ALLOWED
 		final user:String = request[0].username;
 		final discriminator:Int = Std.parseInt(request[0].discriminator);
 
-        for (index => button in CoolVars.data.discordButtons)
-        {
-            if (index > 1)
-                break;
-                
-            var btn:DiscordButton = new DiscordButton();
-            
-            btn.label = button.label ?? 'Button';
-            btn.url = button.url ?? 'https://ale-psych-crew.github.io/ALE-Psych-Website';
+		for (index => button in CoolVars.data.discordButtons)
+		{
+			if (index > 1)
+				break;
 
-            presence.buttons[index] = btn;
-        }
+			var btn:DiscordButton = new DiscordButton();
 
-        updatePresence();
+			btn.label = button.label ?? 'Button';
+			btn.url = button.url ?? 'https://ale-psych-crew.github.io/ALE-Psych-Website';
 
-	    debugTrace('Connected to User ' + (discriminator == 0 ? user : user + ' #' + discriminator), DISCORD);
+			presence.buttons[index] = btn;
+		}
+
+		updatePresence();
+
+		debugTrace('Connected to User ' + (discriminator == 0 ? user : user + ' #' + discriminator), DISCORD);
 		#end
-    }
+	}
 
 	private static function onDisconnected(errorCode:Int, message:ConstCharStar):Void
 	{
