7f853cffde35ce7fc4121908400ba87d0154ae01
AlejoGDOfficial <alejogdoff@gmail.com>
2026-05-08 19:55:53 -0500

    wanna play fornite

--- source/core/structures/JsonData.hx
+++ source/core/structures/JsonData.hx
@@ -8,7 +8,7 @@ class JsonData
     public var developerMode:Bool = true;
     public var hotReloading:Bool = true;
     public var debugPrint:Bool = true;
-    public var verbose:Bool = true;
+    public var verbose:Bool = false;
 
     public var title:String = 'Friday Night Funkin\': ALE Psych';
     public var windowColor:String = '0xFF212121';
--- source/utils/CoolUtil.hx
+++ source/utils/CoolUtil.hx
@@ -2,11 +2,11 @@ package utils;
 
 @:build(core.macros.FunctionsMergeMacro.build(
 	[
-		'utils.cool.CameraUtil',
 		'utils.cool.ColorUtil',
 		'utils.cool.StateUtil',
 		'utils.cool.MathUtil',
 		'utils.cool.AppUtil',
+		'utils.cool.CameraUtil',
 		'utils.cool.MapUtil'
 	]
 ))
