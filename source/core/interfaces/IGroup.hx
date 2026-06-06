4caa0fde94b18b1f9e15ae8303f284afb109f931
AlejoGDOfficial <alejogdoff@gmail.com>
2026-06-05 20:38:40 -0500

    ups

--- source/core/interfaces/IGroup.hx
+++ source/core/interfaces/IGroup.hx
@@ -9,4 +9,6 @@ interface IGroup extends IFlxDestroyable
     public function add(obj:FlxBasic):FlxBasic;
     public function insert(index:Int, obj:FlxBasic):FlxBasic;
     public function remove(obj:FlxBasic, destroy:Bool = false):FlxBasic;
+
+    public function forEachAlive(func:FlxBasic -> Void, recurse:Bool = false):Void;
 }
--- source/core/interfaces/IState.hx
+++ source/core/interfaces/IState.hx
@@ -5,6 +5,4 @@ import flixel.FlxBasic;
 interface IState extends IGroup
 {
     public var updating(get, never):Bool;
-
-    public function forEachAlive(func:FlxBasic -> Void, recurse:Bool = false):Void;
 }
