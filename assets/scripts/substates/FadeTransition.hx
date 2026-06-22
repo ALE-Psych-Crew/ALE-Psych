7094c43ef51dd4d26c21086b47dcab3d4f4de79c
AlejoGDOfficial <alejogdoff@gmail.com>
2026-06-22 10:49:53 -0500

    omg assets

---
+++ assets/scripts/substates/FadeTransition.hx
@@ -0,0 +1,48 @@
+import flixel.util.FlxGradient;
+
+import flixel.FlxState;
+
+var finishCallback:Void -> Void;
+
+var transGradient:FlxSprite;
+var transBlack:FlxSprite;
+
+var transIn:Bool;
+
+function new(trnsIn:Bool, ?callback:Void -> Void)
+{
+	transIn = trnsIn;
+
+	finishCallback = callback;
+
+	FlxState.transitioning = true;
+	
+	transGradient = FlxGradient.createGradientFlxSprite(FlxG.width, FlxG.height, (transIn ? [FlxColor.BLACK, 0x0] : [0x0, FlxColor.BLACK]));
+	transGradient.scrollFactor.set();
+	add(transGradient);
+	transGradient.cameras = [subCamera];
+	transGradient.y = -transGradient.height;
+
+	transBlack = transBlack = new FlxSprite().makeGraphic(FlxG.width, FlxG.height + (transIn ? 400 : 0), FlxColor.BLACK);
+	transBlack.scrollFactor.set();
+	add(transBlack);
+	transBlack.cameras = [subCamera];
+}
+
+function onUpdate(elapsed:Float)
+{
+	transGradient.y += (transGradient.height + FlxG.height) * elapsed / 0.5;
+	
+	transBlack.y = transGradient.y + (transIn ? -1 : 1) * transBlack.height; 
+
+	if (transGradient.y >= FlxG.height)
+	{
+		close();
+
+		if (transIn)
+			finishCallback();
+	}
+}
+
+function onDestroy()
+	FlxState.transitioning = false;
