9a90aeba9aa2b4a29f7b87876322d365dbff59a8
AlejoGDOfficial <alejogdoff@gmail.com>
2026-06-24 12:51:07 -0500

    merge

---
+++ source/core/structures/Object.hx
@@ -0,0 +1,3 @@
+package core.structures;
+
+typedef Object = {}
--- source/funkin/config/SaveFile.hx
+++ source/funkin/config/SaveFile.hx
@@ -1,25 +1,34 @@
 package funkin.config;
 
+import core.structures.Object;
+
 import lime.system.System;
+
+import haxe.io.Path;
 
 import sys.FileSystem;
 import sys.io.File;
 
-import haxe.io.Path;
+import Type;
 
 class SaveFile
 {
     public final path:String;
 
-    public var data:Dynamic = {};
+    public var data(default, set):Object = {};
+    function set_data(value:Object):Object
+        return merge(value);
 
     public function new(id:String)
     {
         path = Path.join([System.applicationStorageDirectory, Paths.mod, id + '.json']);
 
+        load();
+    }
+
+    public function load()
         if (FileSystem.exists(path))
             data = Json.parse(File.getContent(path));
-    }
 
     public function save()
     {
@@ -34,4 +43,25 @@ class SaveFile
     public function delete()
         if (FileSystem.exists(path))
             FileSystem.deleteFile(path);
+
+    public function merge(newData:Object, ?original:Object):Object
+    {
+        original ??= data;
+
+        function isObject(obj:Dynamic)
+            return Type.typeof(obj) == ValueType.TObject;
+
+        for (field in Reflect.fields(newData))
+        {
+            final originalRes:Dynamic = Reflect.field(original, field);
+            final newRes:Dynamic = Reflect.field(newData, field);
+
+            if (isObject(originalRes) && isObject(newRes))
+                merge(newRes, originalRes);
+            else
+                Reflect.setField(original, field, newRes);
+        }
+
+        return original;
+    }
 }
