a897768be7df68ec7b351974c2550da62cb82686
AlejoGDOfficial <alejogdoff@gmail.com>
2026-07-09 18:24:48 -0500

    FlxRandom is not FlxRandoming i dunno

--- source/utils/cool/ArrayUtil.hx
+++ source/utils/cool/ArrayUtil.hx
@@ -4,4 +4,7 @@ class ArrayUtil
 {
     public static function setArrayPrefix(arr:Array<String>, prefix:String):Array<String>
         return [for (str in arr) prefix + str];
+
+    public static function getRandomObject(array:Array<Dynamic>):Dynamic
+        return array[FlxG.random.int(0, array.length - 1)];
 }
