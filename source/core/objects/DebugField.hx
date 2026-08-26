84cb59f115e3c86093ffbcc698621800eb637dc0
AlejoGDOfficial <alejogdoff@gmail.com>
2026-08-26 18:36:12 -0500

    typo

--- source/core/objects/DebugField.hx
+++ source/core/objects/DebugField.hx
@@ -7,98 +7,92 @@ import openfl.text.TextFormat;
 /**
  * This corresponds to each of the text boxes found in the `DebugTray`
  */
-class DebugField extends GameObject
-{
-    /**
-     * Displayed text
-     */
-    final label:TextField;
+class DebugField extends GameObject {
+	/**
+	 * Displayed text
+	 */
+	final label:TextField;
 
-    /**
-     * Function that determines the text to be displayed
-     */
-    public var textFunction:Void -> String;
+	/**
+	 * Function that determines the text to be displayed
+	 */
+	public var textFunction:Void->String;
 
-    /**
-     * This creates one of the fields in `DebugTray`
-     * 
-     * @param textFunction Function that determines the text to be displayed
-     */
-    public function new(?textFunction:Void -> String)
-    {
-        super();
+	/**
+	 * This creates one of the fields in `DebugTray`
+	 * 
+	 * @param textFunction Function that determines the text to be displayed
+	 */
+	public function new(?textFunction:Void->String) {
+		super();
 
-        this.textFunction = textFunction ?? () -> '';
+		this.textFunction = textFunction ?? () -> '';
 
-        label = new TextField();
-        label.antiAliasType = ADVANCED;
-        label.sharpness = 200;
-        label.selectable = label.mouseEnabled = false;
-        label.autoSize = LEFT;
-        label.multiline = true;
-        label.defaultTextFormat = new TextFormat(Paths.font('monsterrat.ttf'), 13, FlxColor.WHITE);
-        label.x = 5;
-        label.y = 5;
+		label = new TextField();
+		label.antiAliasType = ADVANCED;
+		label.sharpness = 200;
+		label.selectable = label.mouseEnabled = false;
+		label.autoSize = LEFT;
+		label.multiline = true;
+		label.defaultTextFormat = new TextFormat(Paths.font('montserrat.ttf'), 13, FlxColor.WHITE);
+		label.x = 5;
+		label.y = 5;
 
-        add(label);
+		add(label);
 
-        showBG = true;
+		showBG = true;
 
-        updateField();
-    }
+		updateField();
+	}
 
-    @:dox(hide)
-    var currentWidth:Float = 0;
+	@:dox(hide)
+	var currentWidth:Float = 0;
 
-    @:dox(hide)
-    var currentHeight:Float = 0;
+	@:dox(hide)
+	var currentHeight:Float = 0;
 
-    /**
-     * Determines whether or not the box background should be displayed
-     */
-    public var showBG(default, set):Bool;
-    @:dox(hide)
-    function set_showBG(value:Bool):Bool
-    {
-        showBG = value;
+	/**
+	 * Determines whether or not the box background should be displayed
+	 */
+	public var showBG(default, set):Bool;
 
-        if (!showBG)
-            graphics.clear();
-        else
-            updateField(true);
+	@:dox(hide)
+	function set_showBG(value:Bool):Bool {
+		showBG = value;
 
-        return showBG;
-    }
+		if (!showBG)
+			graphics.clear();
+		else
+			updateField(true);
 
-    override function update(elapsed:Float)
-    {
-        super.update(elapsed);
+		return showBG;
+	}
 
-        updateField();
-    }
+	override function update(elapsed:Float) {
+		super.update(elapsed);
 
-    /**
-     * This updates how the background should be drawn and how the text should be displayed
-     * 
-     * @param drawBG Determines whether or not the box background should be displayed
-     */
-    function updateField(?drawBG:Bool = false)
-    {
-        label.text = textFunction();
+		updateField();
+	}
 
-        if (label.width != currentWidth || label.height != currentHeight || drawBG)
-        {
-            currentWidth = label.width;
-            currentHeight = label.height;
+	/**
+	 * This updates how the background should be drawn and how the text should be displayed
+	 * 
+	 * @param drawBG Determines whether or not the box background should be displayed
+	 */
+	function updateField(?drawBG:Bool = false) {
+		label.text = textFunction();
 
-            if (showBG)
-            {
-                graphics.clear();
-                graphics.lineStyle(2, FlxColor.WHITE, 0.5);
-                graphics.beginFill(FlxColor.BLACK, 0.5);
-                graphics.drawRect(0, 0, currentWidth + 10, currentHeight + 10);
-                graphics.endFill();
-            }
-        }
-    }
+		if (label.width != currentWidth || label.height != currentHeight || drawBG) {
+			currentWidth = label.width;
+			currentHeight = label.height;
+
+			if (showBG) {
+				graphics.clear();
+				graphics.lineStyle(2, FlxColor.WHITE, 0.5);
+				graphics.beginFill(FlxColor.BLACK, 0.5);
+				graphics.drawRect(0, 0, currentWidth + 10, currentHeight + 10);
+				graphics.endFill();
+			}
+		}
+	}
 }
