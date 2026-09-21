16d82fe6d8d7cfd83b7854efecc0b026e50f69a9
AlejoGDOfficial <alejogdoff@gmail.com>
2026-09-21 11:24:10 -0500

    la reflexion no quiere

---
+++ source/core/structures/FlxColorData.hx
@@ -0,0 +1,25 @@
+package core.structures;
+
+typedef FlxColorData = {
+    ?red:Int,
+    ?blue:Int,
+    ?green:Int,
+    ?alpha:Int,
+
+    ?redFloat:Float,
+    ?blueFloat:Float,
+    ?greenFloat:Float,
+    ?alphaFloat:Float,
+
+    ?cyan:Float,
+    ?magenta:Float,
+    ?yellow:Float,
+    ?black:Float,
+
+    ?rgb:FlxColor,
+    ?hue:Float,
+    ?saturation:Float,
+    ?brightness:Float,
+    ?lightness:Float,
+    ?luminance:Float
+}
--- source/utils/cool/ColorUtil.hx
+++ source/utils/cool/ColorUtil.hx
@@ -1,5 +1,7 @@
 package utils.cool;
 
+import core.structures.FlxColorData;
+
 import flixel.util.FlxColor;
 
 using StringTools;
@@ -28,4 +30,89 @@ class ColorUtil
 
 	public static function colorLerp(from:FlxColor, to:FlxColor, ratio:Float):FlxColor
 		return FlxColor.interpolate(from, to, MathUtil.fpsRatio(ratio));
+
+	public static function getColorData(color:FlxColor):FlxColorData
+		return {
+			red: color.red,
+			blue: color.blue,
+			green: color.green,
+			alpha: color.alpha,
+
+			redFloat: color.redFloat,
+			blueFloat: color.blueFloat,
+			greenFloat: color.greenFloat,
+			alphaFloat: color.alphaFloat,
+
+			cyan: color.cyan,
+			magenta: color.magenta,
+			yellow: color.yellow,
+			black: color.black,
+
+			rgb: color.rgb,
+			hue: color.hue,
+			saturation: color.saturation,
+			brightness: color.brightness,
+			lightness: color.lightness,
+			luminance: color.luminance
+		};
+
+	public static function setColorData(color:FlxColor, data:FlxColorData):FlxColor
+	{
+		if (data.red != null)
+			color.red = data.red;
+		
+		if (data.blue != null)
+			color.blue = data.blue;
+		
+		if (data.green != null)
+			color.green = data.green;
+		
+		if (data.alpha != null)
+			color.alpha = data.alpha;
+		
+
+		if (data.redFloat != null)
+			color.redFloat = data.redFloat;
+		
+		if (data.blueFloat != null)
+			color.blueFloat = data.blueFloat;
+		
+		if (data.greenFloat != null)
+			color.greenFloat = data.greenFloat;
+		
+		if (data.alphaFloat != null)
+			color.alphaFloat = data.alphaFloat;
+		
+
+		if (data.cyan != null)
+			color.cyan = data.cyan;
+		
+		if (data.magenta != null)
+			color.magenta = data.magenta;
+		
+		if (data.yellow != null)
+			color.yellow = data.yellow;
+		
+		if (data.black != null)
+			color.black = data.black;
+		
+
+		if (data.rgb != null)
+			color.rgb = data.rgb;
+		
+		if (data.hue != null)
+			color.hue = data.hue;
+		
+		if (data.saturation != null)
+			color.saturation = data.saturation;
+		
+		if (data.brightness != null)
+			color.brightness = data.brightness;
+		
+		if (data.lightness != null)
+			color.lightness = data.lightness;
+		
+
+		return color;
+	}
 }
