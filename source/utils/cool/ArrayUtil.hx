cdd6e269c47baffe5337954dcef1097d7532dbc6
AlejoGDOfficial <alejogdoff@gmail.com>
2026-09-24 21:27:19 -0500

    ArrayUtil.getRandomObject excludes

--- source/utils/cool/ArrayUtil.hx
+++ source/utils/cool/ArrayUtil.hx
@@ -5,6 +5,24 @@ class ArrayUtil
     public static function setArrayPrefix(arr:Array<String>, prefix:String):Array<String>
         return [for (str in arr) prefix + str];
 
-    public static function getRandomObject(array:Array<Dynamic>):Dynamic
-        return array[FlxG.random.int(0, array.length - 1)];
+    public static function getRandomObject(array:Array<Dynamic>, ?excludes:Array<Dynamic>):Dynamic
+    {
+        var res:Dynamic = null;
+
+        var intExcludes:Array<Int> = [];
+
+        while (intExcludes.length < array.length)
+        {
+            final int:Int = FlxG.random.int(0, array.length - 1, intExcludes);
+
+            res = array[int];
+
+            if (excludes != null && excludes.contains(res))
+                intExcludes.push(int);
+            else
+                break;
+        }
+
+        return res;
+    }
 }
